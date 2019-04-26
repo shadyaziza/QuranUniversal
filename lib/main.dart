14640e0f31e1938856c7206f3475f80e484486bf
shadyaziza <shadyaziza@gmail.com>
2019-04-26 13:22:57 +0200

    Fix inconsistant sajda type

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import './src/ui/home/home.dart';
 
 void main() => runApp(Root());
@@ -6,12 +7,17 @@ void main() => runApp(Root());
 class Root extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       theme: ThemeData(
           primarySwatch: Colors.blueGrey,
           fontFamily: 'Hafs',
           textTheme: TextTheme(
-              body1: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold))),
+              body1: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold))),
       home: Scaffold(
           appBar: AppBar(
             title: Text('Quran Universal'),
--- lib/src/ui/surah/surah.dart
+++ lib/src/ui/surah/surah.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../blocs/single_surah/bloc.dart';
@@ -54,46 +56,50 @@ class _SurahScreenState extends State<SurahScreen> {
             );
           } else {
             return Scaffold(
-              appBar: AppBar(
-                iconTheme: Theme.of(context)
-                    .iconTheme
-                    .copyWith(color: Theme.of(context).primaryColor),
-                backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-                centerTitle: true,
-                elevation: 0.0,
-                title: Text(
-                  state.surah.name,
-                  textScaleFactor: 1.8,
-                  style: TextStyle(color: Theme.of(context).primaryColor),
-                ),
-              ),
-              body: Directionality(
-                textDirection: TextDirection.rtl,
-                child: SingleChildScrollView(
-                  child: Container(
-                    margin: const EdgeInsets.all(12.0),
-                    decoration: BoxDecoration(border: Border.all(width: 2.0)),
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text.rich(
-                      TextSpan(
-                        text:
-                            '${state.surah.ayahs[0].text} ${state.surah.ayahs[0].numberInSurah} ',
-                        children: List.generate(
-                          state.surah.ayahs.length - 1,
-                          (int index) {
-                            return TextSpan(
-                                text:
-                                    '${state.surah.ayahs[index].text} ${state.surah.ayahs[index].numberInSurah} ');
-                          },
-                        ),
-                      ),
-                      textAlign: TextAlign.justify,
-                      textDirection: TextDirection.rtl,
-                    ),
+                appBar: AppBar(
+                  iconTheme: Theme.of(context)
+                      .iconTheme
+                      .copyWith(color: Theme.of(context).primaryColor),
+                  backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                  centerTitle: true,
+                  elevation: 0.0,
+                  title: Text(
+                    state.surah.name,
+                    textScaleFactor: 1.5,
+                    style: TextStyle(color: Theme.of(context).primaryColor),
                   ),
                 ),
-              ),
-            );
+                body: Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: SingleChildScrollView(
+                    child: Container(
+                      margin: const EdgeInsets.all(12.0),
+                      decoration: BoxDecoration(border: Border.all(width: 2.0)),
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text.rich(
+                        TextSpan(
+                          text:
+                              '${state.surah.ayahs[0].text} ${state.surah.ayahs[0].numberInSurah} ',
+                          children: List.generate(
+                            state.surah.ayahs.length - 1,
+                            (int index) {
+                              return TextSpan(
+                                  text: '${state.surah.ayahs[index + 1].text}',
+                                  children: [
+                                    TextSpan(
+                                        text:
+                                            ' ${state.surah.ayahs[index + 1].numberInSurah} ',
+                                        style: TextStyle(color: Colors.red))
+                                  ]);
+                            },
+                          ),
+                        ),
+                        textAlign: TextAlign.start,
+                        textDirection: TextDirection.rtl,
+                      ),
+                    ),
+                  ),
+                ));
           }
         }
       },
